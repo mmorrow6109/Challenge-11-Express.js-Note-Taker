2f47c86f28379b6313d0ad93dcae928d4e1d0de4
mmorrow6109 <msmorrow6109@gmail.com>
2024-01-27 16:22:10 -0800

    Everything functional except for server

--- Develop/Routes/api-routes.js
+++ Develop/Routes/api-routes.js
@@ -2,15 +2,20 @@ const router = require("express").Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
-router.get('api/notes', async (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    const newNote = {
+router.get('/api/notes', async (req, res) => {
+    const dbGet = await fs.readFileSync('db/db.json', 'utf8');
+    res.json(dbGet);
+});
+
+router.post('/api/notes', (req, res) => {
+    const dbPost = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    const newFeedback = {
         title: req.body.title,
         text: req.body.text,
         id: uuidv4()
     }
-    dbJSON.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(dbJSON));
+    dbJSON.push(newFeedback);
+    fs.writeFileSync('./db/db.json', JSON.stringify(dbPost));
     res.json(dbJSON);
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const html_routes = require('./Routes/html-routes');
-const api_routes = require('./Routes/api-routes');
-
-const app = express();
+const htmlRoutes = require('./Routes/html-routes.js');
+const apiRoutes = require('./Routes/api-routes.js');
 
 const port = process.env.PORT || 3001;
+const app = express();
 
 //middleware
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/', html_routes);
-app.use('/api', api_routes);
+app.use(express.static('Develop/Public'));
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`Server listening on port ${port}! Visit http://localhost:${port} in your browser!`);
 });
 
 // Route for getting the home page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'Public', 'index.html')));
